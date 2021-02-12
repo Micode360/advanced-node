8c945cd4d485414b693cd607c4761b575b113c29
KING <abayemiracle@gmail.com>
2021-02-12 13:19:55 +0100

    finished project

---
+++ model/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    username: {
+        type: String, 
+        required: true 
+    }
+},{
+    timestamps:true,
+});
+
+
+
+
+const userModel = mongoose.model('userModel', userSchema);
+
+module.exports = userModel;
